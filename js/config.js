5b55e6e854b69b68f05758017eddceb4ba489971
Rajaram <rr.kaliyaperumal@gmail.com>
2019-07-29 13:26:19 +0200

    First commit. Copied source code from rd connect demonstrator

---
+++ js/config.js
@@ -0,0 +1,53 @@
+var SPARQL_ENDPOINT = 'http://training.fairdata.solutions/sparql?query='
+
+var TEMPLATE_QUERIES = {
+        1 : {
+		text : 'A user of the virtual platform (VP) wants to discover all patient registries for a particular country', 
+                variables: ['country'], 
+                query : readFile('sparqlQueries/template1.sparql', 'text')
+		
+            },   
+	2 : {
+		text : 'A user of the virtual platform (VP) wants to discover all patient registries that deal with specific disease', 
+                variables: ['disease'], 
+                query : readFile('sparqlQueries/template2.sparql', 'text')
+            },
+            
+  3 : {
+              text : 'A user of the virtual platform (VP) wants to discover all patient registries for a particular disease in their country', 
+                          variables: ['disease', 'country'], 
+                          query : readFile('sparqlQueries/template1.sparql', 'text')
+              
+                      },          
+    };
+
+ 
+
+var VARIABLE_QUERIES = {
+        country: readFile('sparqlQueries/getCountries.sparql', 'text'),
+        disease: readFile('sparqlQueries/getDiseases.sparql', 'text')
+
+}
+
+function readFile(url, dataType) {
+    var content = null;    
+    $.ajax({            
+        url: url,            
+        async: false,   
+        cache: false,
+        dataType: dataType,
+        success: function(response) {   
+            content = response;  
+            console.log("Content of the file < " + url +"> is read successful")
+        },
+        error: function(xhr){
+            var errorMsg = ("An error reading file : " + xhr.status + " " + 
+                    xhr.statusText);
+            alert(errorMsg);
+            console.log(errorMsg)
+        }
+    });       
+    return content;
+}
+
+
