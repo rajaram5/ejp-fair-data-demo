42ff555d0320bc5e87a7822e9c5280f23a654e1a
Rajaram <rr.kaliyaperumal@gmail.com>
2019-07-30 12:01:10 +0200

    SPARQL query file exe is changed

--- js/config.js
+++ js/config.js
@@ -4,27 +4,27 @@ var TEMPLATE_QUERIES = {
   1 : {
     text : 'A user of the virtual platform (VP) wants to discover all patient registries for a particular country',
     variables : [ 'country', 'registryType'],
-    query : readFile('sparqlQueries/template1.sparql', 'text')
+    query : readFile('sparqlQueries/template1.rq', 'text')
 
   },
   2 : {
     text : 'A user of the virtual platform (VP) wants to discover all patient registries that deal with specific disease',
     variables : [ 'disease', 'registryType'],
-    query : readFile('sparqlQueries/template2.sparql', 'text')
+    query : readFile('sparqlQueries/template2.rq', 'text')
   },
 
   3 : {
     text : 'A user of the virtual platform (VP) wants to discover all patient registries for a particular disease in their country',
     variables : [ 'disease', 'country', 'registryType'],
-    query : readFile('sparqlQueries/template1.sparql', 'text')
+    query : readFile('sparqlQueries/template1.rq', 'text')
 
   },
 };
 
 var VARIABLE_QUERIES = {
-  country : readFile('sparqlQueries/getCountries.sparql', 'text'),
-  disease : readFile('sparqlQueries/getDiseases.sparql', 'text'),
-  registryType : readFile('sparqlQueries/getRegistryType.sparql', 'text')
+  country : readFile('sparqlQueries/getCountries.rq', 'text'),
+  disease : readFile('sparqlQueries/getDiseases.rq', 'text'),
+  registryType : readFile('sparqlQueries/getRegistryType.rq', 'text')
 
 }
 
